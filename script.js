51f829301f942f1df34c95b7aef0048d1d207639
majauduks <93546860+majauduks@users.noreply.github.com>
2021-12-19 20:53:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let phoneNumber;
+phoneNumber= 30;
+let priceRate;
+priceRate = 5;
+let phonePrice;
+phonePrice = 119.95;
+let priceRateDecimal;
+priceRateDecimal = priceRate/100;;
+
+let fullPrice;
+
+fullPrice = phoneNumber*phonePrice*priceRateDecimal;
+console.log(fullPrice);
+
+
+let pi=3.14;
+let area;
+let r;
+r=prompt("Кој е радиусот на кругот?");
+area=pi*r**2;
+console.log(area);
---
+++ script2.js
@@ -0,0 +1,58 @@
+let zodiac;
+let yearOfBirth;
+yearOfBirth = prompt("Vnesi godina na ragjanje");
+zodiac= (yearOfBirth-4) % 12;
+
+if (zodiac===0)
+{
+    console.log("Rat");
+}
+ else if (zodiac===1)
+ {
+     console.log("Ox");
+ }
+ else if (zodiac===2)
+ {
+     console.log("Tiger");
+ }
+ else if (zodiac===3)
+ {
+     console.log("Rabbit");
+ }
+ else if (zodiac===4)
+ {
+     console.log("Dragon");
+ }
+ else if (zodiac===5)
+ {
+     console.log("Snake");
+ }
+ else if (zodiac===6)
+ {
+     console.log("Horse");
+ }
+ else if (zodiac===7)
+ {
+     console.log("Goat");
+ }
+ else if (zodiac===8)
+ {
+     console.log("Monkey");
+ }
+ else if (zodiac===9)
+ {
+     console.log("Rooster");
+ }
+ else if (zodiac===10)
+ {
+     console.log("Dog");
+ }
+ else if (zodiac===11)
+ {
+     console.log("Pig");
+ }
+
+ else 
+ {
+     console.log("Внесовте невалиден број");
+ }
